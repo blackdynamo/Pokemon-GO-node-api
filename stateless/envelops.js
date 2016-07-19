f403ab6a0ab4d5442c0242037fb77556ab55d0c3
blackdynamo <d16345401@gmail.com>
2016-07-18 21:28:53 -0600

    fixing the envelops

--- stateless/envelops.js
+++ stateless/envelops.js
@@ -1,6 +1,10 @@
 var protoBuf = require("protobufjs");
 
-var builder = protoBuf.loadProtoFile("./pokemon.proto");
+var builder = protoBuf.loadProtoFile("pokemon.proto");
+if (builder === null) {
+    builder = protoBuf.loadProtoFile("./node_modules/pokemon-go-node-api/pokemon.proto");
+}
+
 var pokemonProto = builder.build();
 
 module.exports = {
